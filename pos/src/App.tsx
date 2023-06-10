0a9301989d65b1c4cc14ea25f6209ec2ac2dfed9
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-06-10 17:08:22 +0530

    creating customers table view

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Customers from './pages/customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
--- pos/src/pages/customers.tsx
+++ pos/src/pages/customers.tsx
@@ -1,9 +1,50 @@
-import React from "react";
 
-function Customers() {
-    return (
-        <div>customers</div>
-    )
+import { Space, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import './customer.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface DataType {
+    _id: string;
+    name: string;
 }
 
-export default Customers;
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_, record) => (
+            <Space size="middle">
+                <a>Invite {record.name}</a>
+                <a>Delete</a>
+            </Space>
+        ),
+    },
+];
+
+export default function Customers() {
+    const [customerDate, setCustomerDate] = useState<DataType[] | undefined>(undefined);
+
+    const getCustomers = async () => {
+        const { data } = await axios.get('http://localhost:9090/customer/');
+        console.log(data.customers)
+        setCustomerDate(data.customers)
+    }
+
+    useEffect(() => {
+        getCustomers()
+    }, [])
+
+    return (
+        <div className='container'>
+            <Table columns={columns} dataSource={customerDate} />
+        </div>
+    )
+}
