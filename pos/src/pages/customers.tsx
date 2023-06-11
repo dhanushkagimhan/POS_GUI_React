5adb460098059d4fdddb55c6ea22b49fd7d2b4ba
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-06-11 13:05:11 +0530

    implemented delete method

--- pos/src/pages/customers.tsx
+++ pos/src/pages/customers.tsx
@@ -5,33 +5,34 @@ import './customer.scss'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { DeleteOutlined } from '@ant-design/icons';
 
 interface DataType {
     _id: string;
     name: string;
 }
 
-const columns: ColumnsType<DataType> = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
-
 export default function Customers() {
     const [customerDate, setCustomerDate] = useState<DataType[] | undefined>(undefined);
+
+    const columns: ColumnsType<DataType> = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Invite {record.name}</a>
+                    <span><DeleteOutlined onClick={() => deleteCustomer(record)} style={{ color: "red" }} /></span>
+                </Space>
+            ),
+        },
+    ];
 
     const getCustomers = async () => {
         const { data } = await axios.get('http://localhost:9090/customer/');
@@ -41,6 +42,12 @@ export default function Customers() {
     useEffect(() => {
         getCustomers()
     }, [])
+
+    const deleteCustomer = (value: DataType) => {
+        const response = axios.delete('http://localhost:9090/customer/' + value._id);
+        response.then(() => getCustomers())
+            .catch((error) => console.log(error))
+    }
 
     return (
         <div className='container'>
